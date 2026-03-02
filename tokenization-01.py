30e6a92a9d0d703dfd69e3d7dc7c50445d4f3563
Jonata Albuquerque <jonata.a@outlook.com>
2026-03-02 16:35:37 -0300

    Add sentence tokenization and implement document preprocessing

--- tokenization-01.py
+++ tokenization-01.py
@@ -9,3 +9,16 @@ print(word_tokens)
 
 sentece_tokens = nltk.sent_tokenize(text, language='english')
 print(sentece_tokens)
+
+def preprocess(text):
+    word_tokens = nltk.word_tokenize(text.lower(), language='english')
+    return [word for word in word_tokens if word.isalnum()]
+
+documents = [
+    "This is the first document.",
+    "This document is the second document.",
+    "And this is the third one.",
+]
+
+preprocessed_documents = [" ".join(preprocess(doc)) for doc in documents]
+print(preprocessed_documents)
